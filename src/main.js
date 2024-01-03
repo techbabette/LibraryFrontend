41b9d23c977d7c739df42321fdc3469cf8a24b86
techbabette <marinakrsticrf@gmail.com>
2024-01-03 14:14:09 +0100

    Move directive folder

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router/router'
 import store from './store/store'
-import showToDirective from './components/directives/showToDirective'
+import showToDirective from './directives/showToDirective'
 Vue.config.productionTip = false
 
 Vue.directive("showto", showToDirective);
