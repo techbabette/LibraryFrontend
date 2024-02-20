e0df497adeaf1876775e55a55ea0c871d05634c0
techbabette <marinakrsticrf@gmail.com>
2024-02-20 09:35:54 +0100

    Dashboard chart

--- src/components/PageAdminDashboard.vue
+++ src/components/PageAdminDashboard.vue
@@ -1,10 +1,12 @@
 <script>
 import InfoBlock from '@/components/InfoBlock.vue';
+import ChartBar from './ChartBar.vue';
 export default {
     name : "PageAdminDashboard",
 
     components : {
-        InfoBlock
+        InfoBlock,
+        ChartBar
     },
 
     data(){
@@ -13,7 +15,44 @@ export default {
                 "activeLoans" : 0,
                 "lateLoans" : 0,
                 "newLoans" : 0,
-                "newUsers" : 0
+                "totalLoans" : 0
+            },
+            ChartData : [],
+            chartOptions : {
+                plugins: {
+                    title: {
+                        display: true,
+                        text: 'Loans by Category'
+                    },
+                    colors : {
+                        enabled: true
+                    }
+                },
+                scales : {
+                    x : {
+                        title : {
+                            text : "Category",
+                            display : true,
+                            color : "#911"
+                        }
+                    },
+                    y : {
+                        title : {
+                            text : "Number of loans",
+                            display : true,
+                            color : "#911"
+                        },
+                        ticks : {
+                            beginAtZero: true,
+                            callback: function(val) {
+                                // when the floored value is the same as the value we have a whole number
+                                if (Math.floor(val) === val) {
+                                    return val;
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
     },
@@ -21,6 +60,24 @@ export default {
     computed:{
         adminName: function(){
             return this.$store.getters['user/claims'].name;
+        },
+        chartDataLabels: function(){
+            return this.ChartData.map((el) => el.text);
+        },
+        chartDatasetActive : function(){
+            return {data :this.ChartData.map((el) => el.active_loans_count), label : "Active loans (Includes late)", backgroundColor : "#0DCAF0", hidden: true,};
+        },
+        chartDatasetLate : function(){
+            return {data :this.ChartData.map((el) => el.late_loans_count), label : "Late loans", backgroundColor: "#DC3545", hidden: true,};
+        },
+        chartDatasetNew : function(){
+            return {data :this.ChartData.map((el) => el.new_loans_count), label : "New loans (last 24h)", backgroundColor : "#0D6EFD" , hidden: true,};
+        },
+        chartDatasetAll : function(){
+            return {data :this.ChartData.map((el) => el.loans_count), label : "All-time loans",  backgroundColor: "#FFC107"};
+        },
+        chartDatasets : function(){
+            return [this.chartDatasetAll, this.chartDatasetActive, this.chartDatasetNew, this.chartDatasetLate,];
         }
     },
 
@@ -33,20 +90,21 @@ export default {
             let yesterdayDate = new Date();
             yesterdayDate.setDate(yesterdayDate.getDate() - 1);
             yesterdayDate = yesterdayDate.toISOString();
-            let [newUsers, newLoans, activeLoans, lateLoans, categoriesWithLoans] = await Promise.all([
-                this.$store.dispatch("fetch", {url : "/user", params : {onlyCount : true, since : yesterdayDate}}),
+            let [totalLoans, newLoans, activeLoans, lateLoans, categoriesWithLoans] = await Promise.all([
+                this.$store.dispatch("fetch", {url : "/loan", params : {onlyCount : true, currentAndPrevious : true}}),
                 this.$store.dispatch("fetch", {url : "/loan", params : {onlyCount : true, since : yesterdayDate}}),
                 this.$store.dispatch("fetch", {url : "/loan", params : {onlyCount : true}}),
-                this.$store.dispatch("fetch", {url : "/user", params : {onlyCount : true, late  : true}}),
-                this.$store.dispatch("fetch", {url : "/category", params : {onlyLoanCount : true}}),
+                this.$store.dispatch("fetch", {url : "/loan", params : {onlyCount : true, late  : true}}),
+                this.$store.dispatch("fetch", {url : "/category", 
+                params : {onlyLoanCount : true, withActiveLoanCount : true, withLateLoanCount: true, withNewLoanCount: true}}),
             ])
 
             this.InfoBlocksData.activeLoans = activeLoans.body;
             this.InfoBlocksData.lateLoans = lateLoans.body;
             this.InfoBlocksData.newLoans = newLoans.body;
-            this.InfoBlocksData.newUsers = newUsers.body;
+            this.InfoBlocksData.totalLoans = totalLoans.body;
 
-            console.log(categoriesWithLoans);
+            this.ChartData = categoriesWithLoans.body;
         }
     },
 }
@@ -57,8 +115,8 @@ export default {
         <hr>
         <div class="col-12 d-flex flex-row flex-wrap p-2 align-items-center">
             <div class="col-12 d-flex flex-column col-md-6 justify-content-center">
-                <InfoBlock prop_class="border shadow rounded col-12 col-md-11 my-2 p-2 bg-success text-white text-center"
-                 title="New users" :max_value="this.InfoBlocksData.newUsers"/>
+                <InfoBlock prop_class="border shadow rounded col-12 col-md-11 my-2 p-2 bg-warning text-white text-center"
+                 title="Total loans" :max_value="this.InfoBlocksData.totalLoans"/>
                 <InfoBlock prop_class="border shadow rounded col-12 col-md-11 my-2 p-2 bg-primary text-white text-center"
                  title="New loans" :max_value="this.InfoBlocksData.newLoans"/>
             </div>
@@ -69,6 +127,8 @@ export default {
                  title="Late loans" :max_value="this.InfoBlocksData.lateLoans"/>
             </div>
         </div>
+        <ChartBar id="mychart" title="New chart" class="max-vh-25" style="max-height: 50vh;" :options="chartOptions"
+        :labels="this.chartDataLabels" :datasets="this.chartDatasets"/>
     </div>
 </template>
 
