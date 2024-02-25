bc33fe849f3f6141ea0a355d9c67d92712047de1
techbabette <marinakrsticrf@gmail.com>
2024-02-25 07:22:31 +0100

    Basic message tab

--- src/store/modules/admin.js
+++ src/store/modules/admin.js
@@ -3,19 +3,30 @@ export default {
     namespaced : true,
     state : {
         adminTabs : {
-            "Users" : {
-                "title" : "Users",
-                "weight" : 100,
-                "endpoint" : "user",
+            "Loans" : {
+                "title" : "Loans",
+                "weight" : 90,
+                "endpoint" : "loan?panel=true",
                 "idField" : "id",
                 "tableHeaders" : [
                     {
-                        "text" : "Email",
-                        "field" : "email"
+                        "text" : "User",
+                        "field" : "user.email"
                     },
                     {
-                        "text" : "Registered on",
-                        "field" : "created_at"
+                        "text" : "Book",
+                        "field" : "book.name"
+                    },
+                    {
+                        "text" : "Loaned on",
+                        "field" : "started_at"
+                    },
+                    {
+                        "text" : "Returned on",
+                        "field" : "returned_at",
+                        change : function(item){
+                            return item.returned_at ?? "/";
+                        }
                     }
                 ],
                 "itemOptions" : [
@@ -26,88 +37,16 @@ export default {
                     }
                 ],
                 items : [],
+                searchInputs : {
+                    "since" : {
+                        label : "Loaned after",
+                        field_type : "datetime"
+                    }
+                },
                 searchParams : {
+                    "since" : undefined
                 },
                 "page" : 1,
-                maximumPage : 1,
-            },
-            "Authors" : {
-                "title" : "Authors",
-                "weight" : 95,
-                "endpoint" : "author?withBookCount=true",
-                "showParams" : {},
-                "idField" : "id",
-                "tableHeaders" : [
-                    {
-                        "text" : "Name",
-                        "field" : "name"
-                    },
-                    {
-                        "text" : "Last name",
-                        "field" : "last_name"
-                    },
-                    {
-                        "text" : "Books",
-                        "field" : "books_count"
-                    }
-                ],
-                "itemOptions" : [
-                    {
-                        "name" : "Edit",
-                        "class" : "btn btn-warning mx-1",
-                        "onClick" : "emit:showForm"
-                    }
-                ],
-                items : [],
-                searchInputs : {
-                },
-                searchParams : {
-                },
-                "page" : 1,
-                maximumPage : 1,
-            },
-            "Categories" : {
-                "title" : "Category",
-                "weight" : 95,
-                "endpoint" : "category?withActiveLoanCount=true&withLoanCount=true",
-                "showParams" : {},
-                "idField" : "id",
-                "tableHeaders" : [
-                    {
-                        "text" : "Name",
-                        "field" : "text"
-                    },
-                    {
-                        "text" : "Books",
-                        "field" : "books_count"
-                    },
-                    {
-                        "text" : "Active loans",
-                        "field" : "active_loans_count"
-                    },
-                    {
-                        "text" : "Total loans",
-                        "field" : "loans_count"
-                    }
-                ],
-                "itemOptions" : [
-                    {
-                        "name" : "Edit",
-                        "class" : "btn btn-warning mx-1",
-                        "onClick" : "emit:showForm"
-                    },
-                    {
-                        "name" : "Show books",
-                        "class" : "btn btn-info mx-1",
-                        "onClick" : "dispatch|admin/booksWithCategory"
-                    }
-                ],
-                items : [],
-                searchInputs : {
-                },
-                searchParams : {
-                },
-                page : 1,
                 maximumPage : 1,
             },
             "Books" : {
@@ -157,30 +96,126 @@ export default {
                 page : 1,
                 maximumPage : 1,
             },
-            "Loans" : {
-                "title" : "Loans",
-                "weight" : 90,
-                "endpoint" : "loan?panel=true",
+            "Categories" : {
+                "title" : "Category",
+                "weight" : 95,
+                "endpoint" : "category?withActiveLoanCount=true&withLoanCount=true",
+                "showParams" : {},
                 "idField" : "id",
                 "tableHeaders" : [
                     {
-                        "text" : "User",
-                        "field" : "user.email"
+                        "text" : "Name",
+                        "field" : "text"
                     },
                     {
-                        "text" : "Book",
-                        "field" : "book.name"
+                        "text" : "Books",
+                        "field" : "books_count"
                     },
                     {
-                        "text" : "Loaned on",
-                        "field" : "started_at"
+                        "text" : "Active loans",
+                        "field" : "active_loans_count"
                     },
                     {
-                        "text" : "Returned on",
-                        "field" : "returned_at",
-                        change : function(item){
-                            return item.returned_at ?? "/";
-                        }
+                        "text" : "Total loans",
+                        "field" : "loans_count"
+                    }
+                ],
+                "itemOptions" : [
+                    {
+                        "name" : "Edit",
+                        "class" : "btn btn-warning mx-1",
+                        "onClick" : "emit:showForm"
+                    },
+                    {
+                        "name" : "Show books",
+                        "class" : "btn btn-info mx-1",
+                        "onClick" : "dispatch|admin/booksWithCategory"
+                    }
+                ],
+                items : [],
+                searchInputs : {
+                },
+                searchParams : {
+                },
+                page : 1,
+                maximumPage : 1,
+            },
+            "Authors" : {
+                "title" : "Authors",
+                "weight" : 95,
+                "endpoint" : "author?withBookCount=true",
+                "showParams" : {},
+                "idField" : "id",
+                "tableHeaders" : [
+                    {
+                        "text" : "Name",
+                        "field" : "name"
+                    },
+                    {
+                        "text" : "Last name",
+                        "field" : "last_name"
+                    },
+                    {
+                        "text" : "Books",
+                        "field" : "books_count"
+                    }
+                ],
+                "itemOptions" : [
+                    {
+                        "name" : "Edit",
+                        "class" : "btn btn-warning mx-1",
+                        "onClick" : "emit:showForm"
+                    }
+                ],
+                items : [],
+                searchInputs : {
+                },
+                searchParams : {
+                },
+                "page" : 1,
+                maximumPage : 1,
+            },
+            "Links" : {
+                "title" : "Links",
+                "endpoint" : "link?panel=true",
+                "idField" : "id",
+                "tableHeaders" : [
+                    {
+                        "text" : "Text",
+                        "field" : "text"
+                    },
+                    {
+                        "text" : "Leads to",
+                        "field" : "to"
+                    },
+                    {
+                        "text" : "Access level required",
+                        "field" : "access_level.access_level"
+                    },
+                    {
+                        "text" : "Icon",
+                        "field" : "icon"
+                    }
+                ],
+                items : [],
+                searchParams : {
+                },
+                "page" : 1,
+                maximumPage : 1,
+            },
+            "Users" : {
+                "title" : "Users",
+                "weight" : 100,
+                "endpoint" : "user",
+                "idField" : "id",
+                "tableHeaders" : [
+                    {
+                        "text" : "Email",
+                        "field" : "email"
+                    },
+                    {
+                        "text" : "Registered on",
+                        "field" : "created_at"
                     }
                 ],
                 "itemOptions" : [
@@ -191,18 +226,72 @@ export default {
                     }
                 ],
                 items : [],
-                searchInputs : {
-                    "since" : {
-                        label : "Loaned after",
-                        field_type : "datetime"
-                    }
-                },
                 searchParams : {
-                    "since" : undefined
                 },
                 "page" : 1,
                 maximumPage : 1,
             },
+            "Message Types" : {
+                "title" : "Message types",
+                "weight" : 50,
+                "endpoint" : "messagetype?panel=true",
+                "idField" : "id",
+                "tableHeaders" : [
+                    {
+                        "text" : "Name",
+                        "field" : "name"
+                    },
+                    {
+                        "text" : "Number of messages",
+                        "field" : "messages_count"
+                    }
+                ],
+                "itemOptions" : [
+                    {
+                        "name" : "View messages",
+                        "class" : "btn btn-info mx-1",
+                        "onClick" : "dispatch|admin/messagesOfType"
+                    }
+                ],
+                items : [],
+                searchParams : {
+                },
+                "page" : 1,
+                maximumPage : 1,
+            },
+            "Messages" : {
+                "title" : "Messages",
+                "weight" : 45,
+                "endpoint" : "message",
+                "idField" : "id",
+                "tableHeaders" : [
+                    {
+                        "text" : "Sender",
+                        "field" : "user.email"
+                    },
+                    {
+                        "text" : "Sent at",
+                        "field" : "created_at"
+                    },
+                    {
+                        "text" : "Title",
+                        "field" : "title"
+                    },
+                    {
+                        "text" : "Message type",
+                        "field" : "message_type.name"
+                    },
+                    {
+                        "text" : "Body (Shortened)",
+                        "field" : "body"
+                    }
+                ],
+                items : [],
+                searchParams : {
+                },
+                "page" : 1,
+                maximumPage : 1,
+            }
         },
         currentTab : "Users"
     },
