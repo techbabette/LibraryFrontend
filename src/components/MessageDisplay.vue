803b06c788d937d768b2a8522633878f2beef5b4
techbabette <marinakrsticrf@gmail.com>
2024-03-09 06:04:47 +0100

    Save user token before redirecting

--- src/components/MessageDisplay.vue
+++ src/components/MessageDisplay.vue
@@ -1,15 +1,15 @@
 <script>
-export default{
+export default {
     name: "MessageDisplay",
-    computed:{
-        messages(){
-            return this.$store.getters['messages/get']; 
+    computed: {
+        messages() {
+            return this.$store.getters['messages/get'];
         }
     }
 }
 </script>
 <style scoped>
-#message-holder{
+#message-holder {
     position: fixed;
     bottom: 40px;
     right: 20px;
@@ -18,18 +18,29 @@ export default{
     pointer-events: none;
     text-align: center;
 }
-.disappearing{
+
+.disappearing {
     animation-fill-mode: forwards;
     animation: disappear 5s forwards;
 }
+
 @keyframes disappear {
-    0%   {opacity: 1;}
-    50%  {opacity: 1;}
-    100% {opacity: 0;}
+    0% {
+        opacity: 1;
+    }
+
+    50% {
+        opacity: 1;
+    }
+
+    100% {
+        opacity: 0;
+    }
 }
 </style>
 <template>
     <div id="message-holder" class="d-flex flex-column">
-        <span v-for="message, index in messages" :key="index" class="alert disappearing" :class="{'alert-danger' : !message.success, 'alert-success' : message.success}">{{message.text}}</span>
+        <span v-for="message, index in messages" :key="index" class="alert disappearing"
+            :class="{ 'alert-danger': !message.success, 'alert-success': message.success }">{{ message.text }}</span>
     </div>
 </template>
--- src/components/PageUserLogin.vue
+++ src/components/PageUserLogin.vue
@@ -2,39 +2,39 @@
 import InputAdaptable from "@/components/InputAdaptable.vue";
 import axios from "@/axios/axios";
 export default {
-    name : "PageUserLogin",
+    name: "PageUserLogin",
 
-    components : {
+    components: {
         InputAdaptable
     },
 
-    data(){
+    data() {
         return {
-            loginData : {
-                email : "ilija.krstic.155.21@ict.edu.rs",
-                password : "password"
+            loginData: {
+                email: "ilija.krstic.155.21@ict.edu.rs",
+                password: "password"
             }
         }
     },
 
-    async mounted(){
+    async mounted() {
         let queryOptions = this.$route.query;
         let user = queryOptions.user;
         let token = queryOptions.token;
 
-        if(user && token){
+        if (user && token) {
             let result = await axios.get(`/auth/verify/${user}/${token}`);
 
-            if(result.success){
-                this.$router.push("/").catch(()=>{});
-                this.$store.commit("messages/display", {text : "Successfully activated account", success : true});
+            if (result.success) {
                 this.$store.commit("user/changeToken", result.body);
+                this.$store.commit("messages/display", { text: "Successfully activated account", success: true });
+                this.$router.push("/").catch(() => { });
             }
         }
     },
 
-    methods : {
-        async attemptLogin(){
+    methods: {
+        async attemptLogin() {
             let body = this.loginData;
             await this.$store.dispatch("user/attemptLogin", body);
         }
@@ -45,8 +45,8 @@ export default {
     <div class="mk-solo-page page container-fluid">
         <div class="col-6">
             <h2>Login to your City Library account</h2>
-                <InputAdaptable v-model="loginData.email" field_type="string" label="Email"/>
-                <InputAdaptable v-model="loginData.password" field_type="password" label="Password"/>
+            <InputAdaptable v-model="loginData.email" field_type="string" label="Email" />
+            <InputAdaptable v-model="loginData.password" field_type="password" label="Password" />
             <a href="" @click.prevent="attemptLogin" class="btn btn-light w-100 bigButton my-2">Login</a>
         </div>
     </div>
